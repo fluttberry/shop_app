f7dde0c520e6cffae2160bb59c0a40464dc2edc5
fluttberry <srvffx@gmail.com>
2023-02-25 13:06:21 +0600

    upload screen ongoing

--- lib/app/constants/presentation/main_screen/upload_screen.dart
+++ lib/app/constants/presentation/main_screen/upload_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+
 import 'package:shop_app/app/constants/colors/app_colors.dart';
+import 'package:shop_app/app/utilities/categ_list.dart';
 
 class UploadScreen extends StatefulWidget {
   const UploadScreen({super.key});
@@ -11,6 +11,8 @@ class UploadScreen extends StatefulWidget {
 }
 
 class _UploadScreenState extends State<UploadScreen> {
+  String mainCategoryValue = maincateg.first;
+  String subCategoryValue = women.first;
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -34,15 +36,55 @@ class _UploadScreenState extends State<UploadScreen> {
                 ),
               ),
               Column(
-                children: const [
-                  Text(
+                children: [
+                  const Text(
                     '* select main category',
                     style: TextStyle(
                       color: AppColors.red,
                     ),
                   ),
+                  DropdownButton(
+                    iconSize: 40,
+                    iconDisabledColor: AppColors.red,
+                    dropdownColor: AppColors.yellow.shade400,
+                    value: mainCategoryValue,
+                    items: women.map<DropdownMenuItem<String>>((value) {
+                      return DropdownMenuItem(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? value) {
+                      setState(() {
+                        mainCategoryValue = value!;
+                      });
+                    },
+                  ),
+                  const Text(
+                    '* select subcategory',
+                    style: TextStyle(
+                      color: AppColors.red,
+                    ),
+                  ),
+                  DropdownButton(
+                    iconSize: 40,
+                    iconDisabledColor: AppColors.red,
+                    dropdownColor: AppColors.yellow.shade400,
+                    value: subCategoryValue,
+                    items: women.map<DropdownMenuItem<String>>((value) {
+                      return DropdownMenuItem(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? value) {
+                      setState(() {
+                        subCategoryValue = value!;
+                      });
+                    },
+                  ),
                 ],
-              )
+              ),
             ],
           ),
         ],
@@ -50,3 +92,4 @@ class _UploadScreenState extends State<UploadScreen> {
     );
   }
 }
+//https://www.youtube.com/watch?v=tS5qdZ0frxg 25
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:shop_app/app/constants/presentation/auth/supplier_sign_up.dart';
 import 'package:shop_app/app/constants/presentation/main_screen/customer_screen.dart';
 import 'package:shop_app/app/constants/presentation/main_screen/dashboard_screen.dart';
 import 'package:shop_app/app/constants/presentation/main_screen/supplier_home_screen.dart';
+import 'package:shop_app/app/constants/presentation/main_screen/upload_screen.dart';
 import 'package:shop_app/app/constants/presentation/main_screen/welcome_screen.dart';
 
 void main() async {
@@ -22,10 +23,10 @@ class ShopApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // home: WelcomeScreen(),
+      home: const UploadScreen(),
       // home: const CustomerScreen(),
       // initialRoute: '/customer_screen',
-      initialRoute: '/welcome_screen',
+      // initialRoute: '/welcome_screen',
       routes: {
         '/welcome_screen': (context) => const WelcomeScreen(),
         '/customer_screen': (context) => const CustomerScreen(),
